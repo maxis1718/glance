a528e74c308ece7ab3980acbf7ec17024f899311
Maxis <maxis1718@gmail.com>
2013-12-01 21:47:03 +0800

    add pos API

--- glance.py
+++ glance.py
@@ -4,10 +4,37 @@ from flask import Flask, render_template, request, redirect, Response
 
 app = Flask(__name__)
 
+### ---------------------------- Functions ---------------------------- ###
+def load_json(): return json.load(open('data/bnc.word.filter.json'))
+
+js = load_json()
+
+### ---------------------------- UI ---------------------------- ###
+
 @app.route('/')
 def hello():
-	# return 'XD'
     return render_template('index.html')
+
+@app.route("/pos")
+@app.route("/pos/")
+def pos():
+	return render_template('pos.html')
+
+
+
+### ---------------------------- API ---------------------------- ###
+# pos API
+@app.route('/api/pos/<query>')
+def pos_count(query):
+	return_data = [] if query not in js else js[query]
+	if return_data:
+		r = []
+		for x in return_data:
+			p = x[2]*100
+			p = "%.2f" % p
+			r.append((x[0], x[1], p))
+		return_data = r
+	return Response(json.dumps(return_data), mimetype='application/json')
 
 if __name__ == "__main__":
 	app.debug = True
