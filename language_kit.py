4c1b69b08c14294587dc8e0feefd02c9c76ab3e2
Maxis <maxis1718@gmail.com>
2014-02-09 15:59:12 +0800

    substitute WordNet —> SentiWordNet, add polarity (positive/negative/objective) information

--- language_kit.py
+++ language_kit.py
@@ -1,35 +1,55 @@
-from nltk.corpus import wordnet as wn
+# -*- coding: utf-8 -*-
 
+# substitute WordNet with SentiWordNet
+#from nltk.corpus import wordnet as wn
+import sentiwordnet as sw
+
+# path to the SentiWordNet raw text dump
+_SentiWordNet_path = 'data/SentiWordNet_3.0.0_20130122.txt'
+
+# load SentiWordNet
+swn = sw.SentiWordNetCorpusReader(_SentiWordNet_path)
 
 def query_word( word ):
 
 	output_dic = {}
 
-	synset_lst = wn.synsets( word )
+	senti_synset_lst = swn.senti_synsets(word)
 
-	if( len( synset_lst ) is not 0 ):
-		for syn in synset_lst:
+	if not senti_synset_lst: return output_dic
 
-			syn_dic = {}
+	for senti_syn in senti_synset_lst:
 
-			syn_dic[ 'example' ] = syn.examples
-			syn_dic[ 'definition' ] = syn.definition
-			syn_dic[ 'lemma' ] = syn.lemma_names
-			syn_dic[ 'hypernyms' ] = [ s.name for s in syn.hypernyms() ]
-			syn_dic[ 'hyponyms' ] = [ s.name for s in syn.hyponyms() ]
+		# extract WordNet Synset
+		syn = senti_syn.synset
 
-			output_dic[ syn.name ]  = syn_dic
+		syn_dic = {}
 
+		syn_dic[ 'example' ] = syn.examples
+		syn_dic[ 'definition' ] = syn.definition
+		syn_dic[ 'lemma' ] = syn.lemma_names
+		syn_dic[ 'hypernyms' ] = [ s.name for s in syn.hypernyms() ]
+		syn_dic[ 'hyponyms' ] = [ s.name for s in syn.hyponyms() ]
 
+		syn_dic[ 'polarity' ] = { 'positive': senti_syn.pos_score, 'negative': senti_syn.neg_score, 'objective': senti_syn.obj_score}
+
+		output_dic[ syn.name ]  = syn_dic
 
 	return output_dic
 
 def synset_to_words( syn_name ):
 	return syn_name.split(".")[0]
 
+if __name__ == '__main__':
+	from pprint import pprint
+	
+	while True:
+		print 'input >',
+		query = raw_input()
 
+		pprint(query_word( query ))
 
-print query_word( "dog" )
+		print '='*50
 
 
 
