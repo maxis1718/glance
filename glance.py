107491a71963ccd739b68b288de603efbdd1acb6
Maxis Kao <maxiskao@yahoo-inc.com>
2016-12-08 01:03:07 +0800

    fix indent

--- glance.py
+++ glance.py
@@ -11,7 +11,7 @@ from flask_yeoman import flask_yeoman
 skip_mongo_request = False
 
 if not skip_mongo_request:
-	import mongo as DB  # connect to the MongoDB running on lost
+    import mongo as DB  # connect to the MongoDB running on lost
 
 app = Flask(__name__, static_folder='app/static', template_folder='app/template', static_url_path='')
 app.register_blueprint(flask_yeoman)
@@ -48,29 +48,29 @@ def hello():
 @app.route('/api/word/<query>/usages/')
 @app.route('/api/word/<query>/usages')
 def word_usages(query):
-	pass
+    pass
 
 # @app.route('/api/<query>/pos')
 @app.route('/api/word/<query>/postag/')
 @app.route('/api/word/<query>/postag')
 def pos_count(query):
-	return_data = [] if query not in bnc_pos else bnc_pos[query]
-	if return_data:
-		r = []
-		for x in return_data:
-			p = x[2]*100
-			p = "%.2f" % p
-			r.append((x[0], x[1], p))
-		return_data = r
-	return Response(json.dumps(return_data), mimetype='application/json')
+    return_data = [] if query not in bnc_pos else bnc_pos[query]
+    if return_data:
+        r = []
+        for x in return_data:
+            p = x[2]*100
+            p = "%.2f" % p
+            r.append((x[0], x[1], p))
+        return_data = r
+    return Response(json.dumps(return_data), mimetype='application/json')
 
 
 @app.route('/api/word/<query>/translation/')
 @app.route('/api/word/<query>/translation')
 def translate_first_cht(query):
-	res = None if skip_mongo_request else DB.translation(query)
-	return_data = [] if res == None else res
-	return Response(json.dumps(return_data), mimetype='application/json')
+    res = None if skip_mongo_request else DB.translation(query)
+    return_data = [] if res == None else res
+    return Response(json.dumps(return_data), mimetype='application/json')
 
 
 ### wordnet related 
@@ -79,15 +79,14 @@ def translate_first_cht(query):
 @app.route('/api/sense/<query>/word/')
 @app.route('/api/sense/<query>/word')
 def sense_to_word( query ):
-	return LK.synset_to_words( query )
+    return LK.synset_to_words( query )
 
 # query word info
 # input: word 
 @app.route('/api/word/<query>/')
 @app.route('/api/word/<query>')
 def query_word_info( query ):
-
-	return Response(json.dumps( LK.query_word( query ) ), mimetype='application/json')
+    return Response(json.dumps( LK.query_word( query ) ), mimetype='application/json')
 
 # query hypernym and hyponym and definition of a word 
 # input: sense
@@ -95,60 +94,57 @@ def query_word_info( query ):
 @app.route('/api/sense/<query>/hyponym/')
 @app.route('/api/sense/<query>/hyponym')
 def query_hyponym( query ):
-
-	return Response(json.dumps( LK.query_hyponym( query ) ), mimetype='application/json')
-
-
+    return Response(json.dumps( LK.query_hyponym( query ) ), mimetype='application/json')
 
 ### test API
 # @app.route('/api/test/<query>')
 @app.route('/api/word/<query>/difficulty/')
 @app.route('/api/word/<query>/difficulty')
 def test_level(query):
-	return_data = [] if query not in bnc_test else bnc_test[query]
-	return Response(json.dumps(return_data), mimetype='application/json')
+    return_data = [] if query not in bnc_test else bnc_test[query]
+    return Response(json.dumps(return_data), mimetype='application/json')
 
 ### test API
 # @app.route('/api/wp/<query>')
 @app.route('/api/word/<query>/wp/')
 @app.route('/api/word/<query>/wp')
 def word_position(query, step=34):
-	res = None if skip_mongo_request else DB.position(query)
-	return_data = [] if res == None else res
-	if return_data:
-		nameLst = ["front", "middle", "back"]
-		compact_data = [ [ nameLst[i], sum(return_data[1][i*step:(i+1)*step]) ] for i,x in enumerate(return_data[0][::step]) ]
-	else:
-		compact_data = return_data
-	# return_data = [] if query not in bnc_wp else bnc_wp[query]
-	# return_data = [] if query not in bnc_test else bnc_test[query]
-	return Response(json.dumps(compact_data), mimetype='application/json')
+    res = None if skip_mongo_request else DB.position(query)
+    return_data = [] if res == None else res
+    if return_data:
+        nameLst = ["front", "middle", "back"]
+        compact_data = [ [ nameLst[i], sum(return_data[1][i*step:(i+1)*step]) ] for i,x in enumerate(return_data[0][::step]) ]
+    else:
+        compact_data = return_data
+    # return_data = [] if query not in bnc_wp else bnc_wp[query]
+    # return_data = [] if query not in bnc_test else bnc_test[query]
+    return Response(json.dumps(compact_data), mimetype='application/json')
 
 @app.route('/api/cword/<query>/translation/')
 @app.route('/api/cword/<query>/translation')
 def translate_to_english(query):
-	res = None if skip_mongo_request else DB.translation(query)
-	return_data = [] if res == None else res
-	return Response(json.dumps(return_data), mimetype='application/json')
+    res = None if skip_mongo_request else DB.translation(query)
+    return_data = [] if res == None else res
+    return Response(json.dumps(return_data), mimetype='application/json')
 
 @app.route('/api/word/<query>/genre/')
 @app.route('/api/word/<query>/genre')
 def word_genre(query):
-	res = None if skip_mongo_request else DB.category(query)
-	return_data = [] if res == None else res[0]
-	return Response(json.dumps(return_data), mimetype='application/json')
+    res = None if skip_mongo_request else DB.category(query)
+    return_data = [] if res == None else res[0]
+    return Response(json.dumps(return_data), mimetype='application/json')
 
 @app.route('/api/word/<query>/category/')
 @app.route('/api/word/<query>/category')
 def word_category(query):
-	res = LK.query_category( query )
-	return Response(json.dumps(res), mimetype='application/json')
+    res = LK.query_category( query )
+    return Response(json.dumps(res), mimetype='application/json')
 
 
 if __name__ == "__main__":
-	app.config.update(
-	    DEBUG=False,
-	    SEND_FILE_MAX_AGE_DEFAULT=0
-	)
-	app.run(host="0.0.0.0",port=55688)
+    app.config.update(
+        DEBUG=False,
+        SEND_FILE_MAX_AGE_DEFAULT=0
+    )
+    app.run(host="0.0.0.0",port=55688)
 
